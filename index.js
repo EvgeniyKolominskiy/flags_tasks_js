0c41f5c69f941b479e8e45fabb1f8ba342db4b88
Evgeniy Kolominskiy <kolominskiyy@gmail.com>
2020-08-20 11:49:47 +0300

    Add task 3.

--- index.js
+++ index.js
@@ -35,3 +35,20 @@ function hasElem(array) {
 
 console.log('Task 2');
 console.log(hasElem(arr1));
+
+/*3. Дан массив с числами. Проверьте, что в этом массиве есть число 5. Если есть - выведите 'да', а если нет - выведите 'нет'.*/
+
+console.log('Task 3');
+
+const arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+let flag2 = false;
+for (let i = 0; i < arr2.length; i++) {
+	if (arr2[i] === 5) {
+		flag2 = true;
+	}
+}
+if (flag2) {
+	console.log('да');
+} else {
+	console.log('нет');
+}
