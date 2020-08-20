f7ed553b6b2fcf08c91987a45e5f04346cc5c626
Evgeniy Kolominskiy <kolominskiyy@gmail.com>
2020-08-20 11:43:16 +0300

    Add task 1.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 'use strict';
 
+/*1. дан массив, нужно проверить, есть ли в нем элемент со значением 'c' или нет. Если есть - вывести 'есть', если нет - вывести 'нет'.*/
+console.log('TASK 1');
+
+const arr1 = ['a', 'b', 'c', 'd', 'e',];
+let flag1 = false;
+for (let i = 0; i < arr1.length; i++) {
+	if (arr1[i] === 'c') {
+		flag1 = true;
+	}
+}
+if (flag1) {
+	console.log('есть');
+} else {
+	console.log('нет');
+}
